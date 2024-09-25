e2f40dbba1442214012ec7ce197c12b7924986cb
sabilar-rahman <sabilarrahman36@gmail.com>
2024-09-26 02:50:43 +0600

    vercel issue & cors cookieParser , corsConfig added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
-import express, { Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorhandelers";
 import notFound from "./app/middlewares/notfound";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-const app = express();
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+};
+
+const app: Application = express();
+app.options("*", cors(corsConfig));
+app.use(cors(corsConfig));
 
 // Add body-parser middleware to handle JSON request bodies
 app.use(express.json()); // This will parse incoming JSON requests
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello!, This is car washing system.");
